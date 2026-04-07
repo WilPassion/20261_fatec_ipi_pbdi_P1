f7809a86ce00bf0725b5eab1b1dd6531b9b6e363
aaa <aaaaaa>
2026-04-07 10:11:13 -0300

    feat(p1): encontra sobreviventes da primeira classe

--- script.sql
+++ script.sql
@@ -7,3 +7,27 @@ CREATE TABLE titanic (
     fare DECIMAL(10,2),
     embarked VARCHAR(1)
 );
+
+--Enunciado 2 - Sobrevivência em função da classe social
+DO $$
+DECLARE
+    cur_titanic REFCURSOR;
+    v_passengerid INT;
+    total INT := 0;
+BEGIN
+    OPEN cur_titanic FOR
+        SELECT passengerid
+        FROM titanic
+        WHERE pclass = 1 AND survived = 1;
+
+    LOOP
+        FETCH cur_titanic INTO v_passengerid;
+        EXIT WHEN NOT FOUND;
+
+        total := total + 1;
+    END LOOP;
+
+    CLOSE cur_titanic;
+
+    RAISE NOTICE 'Total de sobreviventes na 1ª classe: %', total;
+END $$;
