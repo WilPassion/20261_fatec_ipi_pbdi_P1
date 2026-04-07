6bc38300343d74cf8a2be345095addbe2d365b4d
aaa <aaaaaa>
2026-04-07 10:16:26 -0300

    feat(p1): encontra sobreviventes do sexo feminino

--- script.sql
+++ script.sql
@@ -31,3 +31,31 @@ BEGIN
 
     RAISE NOTICE 'Total de sobreviventes na 1ª classe: %', total;
 END $$;
+
+-- Enunciado 3 - Sobrevivência em função do gênero
+DO $$
+DECLARE
+    cur_survived REFCURSOR;
+    v_passengerid INT;
+    total INT := 0;
+BEGIN
+    OPEN cur_survived FOR EXECUTE
+        'SELECT passengerid
+         FROM titanic
+         WHERE sex = ''female'' AND survived = 1';
+ 
+    LOOP
+        FETCH cur_survived INTO v_passengerid;
+        EXIT WHEN NOT FOUND;
+ 
+        total := total + 1;
+    END LOOP;
+ 
+    CLOSE cur_survived;
+ 
+    IF total = 0 THEN
+        RAISE NOTICE '%', -1;
+    ELSE
+        RAISE NOTICE 'Total de mulheres sobreviventes é de: %', total;
+    END IF;
+END $$;
